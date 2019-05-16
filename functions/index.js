4cec43b5e257807bec0de4dfbd6ea8238c2678b6
jankcat <timothy.watkins@gmail.com>
2019-05-16 10:54:33 -0600

    basic function to submit entries

---
+++ functions/index.js
@@ -0,0 +1,32 @@
+const admin = require('firebase-admin');
+const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors');
+
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
+const settings = { timestampsInSnapshots: true };
+db.settings(settings);
+const app = express();
+app.use(cors({ origin: true }));
+
+app.post('/', async (req, res) => {
+  if (!req.body.quote) {
+    res.status(400);
+    res.send("Error: No quote provided.");
+    return;
+  }
+  try {
+    const newQuote = {
+      quote: req.body.quote,
+    };
+    await db.collection('quotes').add(newQuote);
+    res.send(`Inserted quote: ${newQuote.quote}`);
+  } catch (e) {
+    console.error(e);
+    res.status(500);
+    res.send(`Error: ${e}`);
+  }
+});
+
+exports.newQuote = functions.https.onRequest(app);
